6a32d432fcb5e46a5d4ea9bce573dbed4fc29716
gulsanamaliqulova77-sudo <gulsanamaliqulova77@gmail.com>
2026-03-16 12:16:07 +0500

    Add get_grade function to determine letter grades

---
+++ function_exercise.py
@@ -0,0 +1,15 @@
+def get_grade(score):
+    if score >= 90:
+         grade = "A"
+    elif score >= 80:
+        grade = "B"
+    elif score >= 70:
+        grade = "C"
+    elif score >= 60:
+        grade = "D"
+    else:
+        grade = "F"
+    return grade
+    
+# Check your answer
+q1.check()
